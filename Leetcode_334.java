c2f15683bf08698bece68f25b0ccfa949c5cfd6a
Touutae-lab <pantakan.totae@gmail.com>
2024-04-16 17:34:55 +0700

    Add 334

---
+++ Leetcode_334.java
@@ -0,0 +1,34 @@
+public class Leetcode_334 {
+    public static void main(String[] args) {
+        int[] nums = new int[] {2,1,5,0,4,6};
+        Solution solution = new Solution();
+        System.out.println(solution.increasingTriplet(nums));
+    }
+}
+
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        if (nums.length < 3) {
+            return false;
+        }
+
+        int smallest = Integer.MAX_VALUE;
+        int middle = Integer.MAX_VALUE;
+        
+        for  (int num : nums) {
+            if (num > middle) {
+                return true;
+            }
+
+            if (num < smallest) {
+                smallest = num;
+            } else if (num > smallest && num < middle) {
+                middle = num;
+            }
+        }
+
+
+        return false;
+    }
+}
