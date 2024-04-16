a5cec421f44dff80d133a634889541448c2e035d
Touutae-lab <pantakan.totae@gmail.com>
2024-04-16 16:56:01 +0700

    add leetcode 151

---
+++ Leetcode_151.java
@@ -0,0 +1,20 @@
+public class Leetcode_151 {
+    
+}
+
+
+class Solution {
+    public String reverseWords(String s) {
+        String[] word = s.split(" ");
+
+        StringBuilder result = new StringBuilder();
+        for (int i = word.length - 1; i >= 0; i--) {
+            if (!word[i].isBlank()) {
+                result.append(word[i]);
+                result.append(" ");
+            }
+        }
+
+        return result.toString().trim();
+    }
+}
